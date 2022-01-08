0d6018fee0a224e2aaa443a21b26ea7bf18d84de
Steve Correa <s.correa@berkeley.edu>
2022-01-07 16:10:55 -0800

    added notes to Landing page component

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -4,10 +4,11 @@ const LandingPage = () => {
 
     /*
     1. include LandingNavSection component as first child of LandingPage ✅
-    2. Create Demo user functionality 
-    3. include SongGallery component as second child of LandingPage
-    4. then, manually add JSX (not a component) for Calling Creators section.
-    5.Last, manually add JSX for footer and include login and Sing up buttons there. 
+    2. Create Demo user functionality ✅
+    3. Style LandingNavSection section TODO:
+    4. include SongGallery component as second child of LandingPage
+    5. then, manually add JSX (not a component) for Calling Creators section.
+    6.Last, manually add JSX for footer and include login and Sing up buttons there. 
      */
     return (
         <div>
