6d51def6abc87b0f00a8375dbfad2978e8e34612
Steve Correa <s.correa@berkeley.edu>
2022-01-09 21:50:29 -0800

    added to upload component. Read TODO

--- frontend/src/components/UploadSong/index.js
+++ frontend/src/components/UploadSong/index.js
@@ -1,12 +1,40 @@
 import "./UploadSong.css"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const UpLoadSong = () => {
 
-    /*
-    
-    
-    */
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState(null)
+    const [song, setSong] = useState(null)
+    const [errors, setErrors] = useState([])
 
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.session.user);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let newErrors = [];
+
+        /*
+
+        TODO: 
+        1. Create song reducer, thunk, and action creator. 
+        2. dispatch create song action to song reducer.
+
+        */
+
+    };
+
+    const addImage = (e) => {
+        const file = e.target.files[0];
+        if (file) setImage(file);
+    }
+
+    const addSong = (e) => {
+        const file = e.target.files[0];
+        if (file) setSong(file);
+    }
 
     return (
         <div className="middleSection">
@@ -20,14 +48,14 @@ const UpLoadSong = () => {
                     <form className="uploadSongForm">
                         <div className="imageBox">
                             <span>Image</span>
-                            <input className="imageInput" type="file" />
+                            <input className="imageInput" type="file" onChange={addImage} />
                         </div>
                         <div className="songAndTitleContainer">
                             <label id="title">Title
                                 <input className="titleInput" type="text" />
                             </label>
                             <label>Song file:
-                                <input className="songInput" type="file" />
+                                <input className="songInput" type="file" onChange={addSong} />
                             </label>
                         </div>
                     </form>
